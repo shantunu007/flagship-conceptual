13f3032343a8c8f8477c3742b50aba2d1d368aba
shantunu007 <chakmashantunu.web@gmail.com>
2025-04-18 20:39:44 +0600

    daisy added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
@@ -10,7 +9,7 @@ function App() {
     <>
      
       <h1>Flagship conceptual</h1>
-      <h1>hello boss</h1>
+      <h1 className='text-xl'>hello boss</h1>
       
 
     </>
